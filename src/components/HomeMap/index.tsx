f04045e98c2f82044cd5fe268056b753fd599db6
Welliton da Conceiçao de Araujo <welliton.araujo@compasso.com.br>
2022-10-01 14:09:54 -0300

    create home screen

---
+++ src/components/HomeMap/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {StyleSheet,Text} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+
+const HomeMap = props => {
+
+    return (
+        <Text style={{height: 300, backgroundColor: '#7899e5', justifyContent: 'center'}}>HomeMap</Text>
+    );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default HomeMap;
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {StyleSheet,Text} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+
+const Home = props => {
+
+    return (
+        <Text>Home</Text>
+    );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default Home;
